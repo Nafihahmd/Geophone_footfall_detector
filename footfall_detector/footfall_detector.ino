c6e12bafa5796b337055dc11168df0d228d0618f
nafihahmd <nafihahamd@gmail.com>
2025-07-30 23:34:46 +0530

    Add ESP-NOW broadcast to gateway functionality

--- footfall_detector/footfall_detector.ino
+++ footfall_detector/footfall_detector.ino
@@ -1,9 +1,110 @@
-void setup() {
-  // put your setup code here, to run once:
+/* 
+  Footfall detector
+  Nafih Ahammed - 2025
 
+  An open‑source solution building and deploying geophone‑based footfall detectors. 
+  Each detector node uses an ESP32‑C6 microcontroller, an ADS1115 16‑bit analog‑to‑digital converter (ADC), 
+  and a TLC272 low‑noise amplifier to sense ground vibrations.
+  Detector nodes communicate wirelessly via ESP‑NOW to a Raspberry Pi control center that collects, 
+  timestamps, and visualizes footfall events in real time. 
+
+*/
+
+#include "ESP32_NOW.h"
+#include "WiFi.h"
+#include "MacAddress.h"
+
+#include <esp_mac.h>  // For the MAC2STR and MACSTR macros
+
+/* Definitions */
+
+#define ESPNOW_WIFI_CHANNEL 6
+
+// Set the MAC address of the device that will receive the data
+// For example: E4:B3:23:B6:A8:08
+const MacAddress peer_mac({0xE4, 0xB3, 0x23, 0xB6, 0xA8, 0x08});
+
+/* Classes */
+
+// Creating a new class that inherits from the ESP_NOW_Peer class is required.
+
+class ESP_NOW_Sensor_Peer : public ESP_NOW_Peer {
+public:
+  // Constructor of the class using the broadcast address
+  ESP_NOW_Sensor_Peer(uint8_t channel, wifi_interface_t iface, const uint8_t *lmk) : ESP_NOW_Peer(peer_mac, channel, iface, lmk) {}
+
+  // Destructor of the class
+  ~ESP_NOW_Sensor_Peer() {
+    remove();
+  }
+
+  // Function to properly initialize the ESP-NOW and register the broadcast peer
+  bool begin() {
+    if (!ESP_NOW.begin() || !add()) {
+      log_e("Failed to initialize ESP-NOW or register the broadcast peer");
+      return false;
+    }
+    return true;
+  }
+
+  // Function to send a message to all devices within the network
+  bool send_message(const uint8_t *data, size_t len) {
+    if (!send(data, len)) {
+      log_e("Failed to broadcast message");
+      return false;
+    }
+    return true;
+  }
+};
+
+/* Global Variables */
+
+uint32_t msg_count = 0;
+
+// Create a broadcast peer object
+ESP_NOW_Sensor_Peer broadcast_peer(ESPNOW_WIFI_CHANNEL, WIFI_IF_STA, nullptr);
+
+/* Main */
+
+void setup() {
+  Serial.begin(115200);
+
+  // Initialize the Wi-Fi module
+  WiFi.mode(WIFI_STA);
+  WiFi.setChannel(ESPNOW_WIFI_CHANNEL);
+  while (!WiFi.STA.started()) {
+    delay(100);
+  }
+
+  Serial.println("ESP-NOW Example - Broadcast Master");
+  Serial.println("Wi-Fi parameters:");
+  Serial.println("  Mode: STA");
+  Serial.println("  MAC Address: " + WiFi.macAddress());
+  Serial.printf("  Channel: %d\n", ESPNOW_WIFI_CHANNEL);
+
+  // Register the broadcast peer
+  if (!broadcast_peer.begin()) {
+    Serial.println("Failed to initialize broadcast peer");
+    Serial.println("Reebooting in 5 seconds...");
+    delay(5000);
+    ESP.restart();
+  }
+
+  Serial.printf("ESP-NOW version: %d, max data length: %d\n", ESP_NOW.getVersion(), ESP_NOW.getMaxDataLen());
+
+  Serial.println("Setup complete. Broadcasting messages every 5 seconds.");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Broadcast a message to all devices within the network
+  char data[32];
+  snprintf(data, sizeof(data), "Hello, World! #%lu", msg_count++);
 
+  Serial.printf("Broadcasting message: %s\n", data);
+
+  if (!broadcast_peer.send_message((uint8_t *)data, sizeof(data))) {
+    Serial.println("Failed to broadcast message");
+  }
+
+  delay(5000);
 }
